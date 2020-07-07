cc9d8b80899780f09eb4c662a4eec0fc965d4a07
Gabi <csgabka@gmail.com>
2020-07-07 13:25:02 +0100

    Updates

---
+++ precache-manifest.e8a2ec53d4d375afe92583e3357037df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b98dfb99c426256f9c9ed858944713",
+    "url": "/React-Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "0809a9ceffbf4553c6a6",
+    "url": "/React-Tic-Tac-Toe/static/css/main.f982477a.chunk.css"
+  },
+  {
+    "revision": "d4bd2f2f98e30a28658c",
+    "url": "/React-Tic-Tac-Toe/static/js/2.4f29a6f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tic-Tac-Toe/static/js/2.4f29a6f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0809a9ceffbf4553c6a6",
+    "url": "/React-Tic-Tac-Toe/static/js/main.33e7a006.chunk.js"
+  },
+  {
+    "revision": "2839713ac66fb09f1754",
+    "url": "/React-Tic-Tac-Toe/static/js/runtime-main.6dbe0009.js"
+  }
+]);
